fe41b7d7d498cb18d948bc643b2ab215eae9bf43
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-10-01 14:52:48 -0400

    feat(component): rendering navbar and login page

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import { Form } from "@/components/login/form";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background p-4">
+      <Form />
+    </div>
+  );
+}
